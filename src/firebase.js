f7052ae627310eda6e742a587d0e160b500a0ab1
Toshmamatov <toshmamatov03@gmail.com>
2022-08-20 13:48:52 -0700

    completing Ui part of the project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAtdUkLlkZ4bmT-ntN-yVPcfja2drgIAPw",
+  authDomain: "clone-37fa5.firebaseapp.com",
+  projectId: "clone-37fa5",
+  storageBucket: "clone-37fa5.appspot.com",
+  messagingSenderId: "798880599055",
+  appId: "1:798880599055:web:36b6cca83efef204bde77a",
+};
